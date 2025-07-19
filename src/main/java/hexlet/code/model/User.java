509a5e7d859ab210590e643af1ba6cc09ffd39b6
Viktor Streltsov <streltsov95va@gmail.com>
2025-07-19 19:59:15 +0300

    feat: create User model

---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,57 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(of = "id")
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ToString.Include
+    private Long id;
+
+    @NotBlank
+    @ToString.Include
+    private String firstName;
+
+    @NotBlank
+    @ToString.Include
+    private String lastName;
+
+    @ToString.Include
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @Size(min = 8)
+    private String password;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+}
