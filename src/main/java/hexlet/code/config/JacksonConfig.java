8f0681dd931930f68d751f5ee0bcf1a8daf49636
Viktor Streltsov <streltsov95va@gmail.com>
2025-11-23 18:18:52 +0300

    fix: add dates serialization to string

--- src/main/java/hexlet/code/config/JacksonConfig.java
+++ src/main/java/hexlet/code/config/JacksonConfig.java
@@ -1,6 +1,7 @@
 package hexlet.code.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,8 @@ public class JacksonConfig {
     Jackson2ObjectMapperBuilder objectMapperBuilder() {
         var builder = new Jackson2ObjectMapperBuilder();
         builder.serializationInclusion(JsonInclude.Include.NON_NULL)
-                .modulesToInstall(new JsonNullableModule());
+                .modulesToInstall(new JsonNullableModule())
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return builder;
     }
 }
