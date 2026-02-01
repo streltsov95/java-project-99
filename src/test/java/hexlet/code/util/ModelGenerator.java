80e18c26edf5bb26e3d7fed2c8755d7ab1ad3ed6
Viktor Streltsov <streltsov95va@gmail.com>
2026-02-01 21:50:03 +0300

    test: add task model to test model generator

--- src/test/java/hexlet/code/util/ModelGenerator.java
+++ src/test/java/hexlet/code/util/ModelGenerator.java
@@ -1,5 +1,6 @@
 package hexlet.code.util;
 
+import hexlet.code.model.Task;
 import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
 import jakarta.annotation.PostConstruct;
@@ -17,6 +18,7 @@ public class ModelGenerator {
 
     private Model<User> userModel;
     private Model<TaskStatus> taskStatusModel;
+    private Model<Task> taskModel;
 
     @Autowired
     private Faker faker;
@@ -25,11 +27,19 @@ public class ModelGenerator {
     private void init() {
         userModel = Instancio.of(User.class)
                 .ignore(Select.field(User::getId))
+                .ignore(Select.field(User::getTasks))
                 .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
                 .toModel();
 
         taskStatusModel = Instancio.of(TaskStatus.class)
                 .ignore(Select.field(TaskStatus::getId))
+                .ignore(Select.field(TaskStatus::getTasks))
+                .toModel();
+
+        taskModel = Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .ignore(Select.field(Task::getAssignee))
+                .ignore(Select.field(Task::getTaskStatus))
                 .toModel();
     }
 }
