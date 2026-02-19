a8327adfaf85574dd6b670414eb29ba2d6c7ddcd
Viktor Streltsov <streltsov95va@gmail.com>
2026-02-19 18:53:49 +0300

    refactor: delete unused import sentryUploadSourceBundleJava task

--- src/main/java/hexlet/code/controller/api/TasksController.java
+++ src/main/java/hexlet/code/controller/api/TasksController.java
@@ -25,8 +25,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/tasks")
 public class TasksController {
